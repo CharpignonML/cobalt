a21399ce91dbc348c62391a57d2f9db9758504ee
Noah Greifer <noah.greifer@gmail.com>
2019-05-29 23:22:38 -0700

    Reminders for experimental branch

---
+++ do_not_include/READ THIS.R
@@ -0,0 +1,4 @@
+#TO DO:
+#Figure out a way to make disp arguments work in print. Probably want a function disp.2dispcols. 
+#Extend base.bal.tab architecture to other base.bal.tabs
+#Make print modular, so they can all rely on print.bal.tab.tab
